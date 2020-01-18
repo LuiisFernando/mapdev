5eec91ddda61344bc30ee2380f38fd2117b8e8d8
Luis Fernando <lsh.fernandors@gmail.com>
2020-01-17 22:10:42 -0300

    implementing frontend with reactjs to add dev consuming api with nodejs a listing devs

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
@@ -11,6 +12,7 @@ mongoose.connect("mongodb+srv://aircnc:aircnc@chamacarreto-kdy6h.mongodb.net/map
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
